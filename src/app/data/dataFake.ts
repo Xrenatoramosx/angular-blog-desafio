20f45005d43fd63af2cc5455b8095503ff02ff83
Xrenatoramosx <renato.ramosnascimento@ufrpe.br>
2023-05-16 23:39:52 -0300

    alterações dos componentes e content.

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,25 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "qual jogos do mega-man Battle Network vale a pena jogar do GBA?",
+    "description": "A série “Mega Man” está no mercado há quase 40 anos. A produção da Capcom estreou no Nintendinho (NES) em 1987. O robozinho azul deveria enfrentar a horda mecânica do Dr. Wily. Dificílimo, o game se tornou um sucesso e rendeu dezenas de continuações.",
+    "photoCover":"https://www.gamecoin.com.br/wp-content/uploads/2023/04/MEGA-MAN-BATTLE-NETWORK-CAPA.jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Castlevania: Lords of Shadow Mirror of Fate completa 10 anos",
+    "description": "Entre as grandes produtoras japonesas, a Konami tem conseguido enterrar um legado rico. Com uma estratégia de mercado confusa, ela tem perdido dinheiro ano após ano e deixado de lado grandes franquias, como “Castlevania”.",
+    "photoCover":"https://www.gamecoin.com.br/wp-content/uploads/2013/11/CASTLEVANIA.jpg"
+  },
+  {
+    "id":"3",
+    "photoCover":"https://www.gamecoin.com.br/wp-content/uploads/2022/12/SUPER-MARIO-LAND-GAMEBOY-GAMEPLAY-14.jpeg",
+    "title":"Super Mario Land: a aventura monocromática do Game Boy",
+    "description":"Quando se faz uma enquete sobre os melhores episódios da franquia “Super Mario”, muito se fala de “Super Mario Bros 3”, assim como “Super Mario World”. Mas pouco se fala de “Super Mario Land”, título produzir para Game Boy, publicado em abril de 1989."},
+  {
+    "id":"4",
+    "photoCover":"https://www.gamecoin.com.br/wp-content/uploads/2021/08/THE-ADDAMS-FAMILY-1.jpg",
+    "title":"30 anos de The Addams Family nos games",
+    "description":"Em novembro de 1991, a Paramount Pictures lançou o longa-metragem “The Addams Family”, que resgatava o seriado dos anos 1960. E junto com o filme a indústria de games também aproveitou o embalo e lançou o game do filme."
   }
 ]
